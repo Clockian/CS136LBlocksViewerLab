2403b9d2a62e7204cae2beb60ce2610259336be0
Clockian <jrsaydyk@gmail.com>
2017-03-07 12:28:29 -0700

    First commit

---
+++ Lab 06/Character.java
@@ -0,0 +1,13 @@
+
+public abstract class Character {
+	private int healthPoints;
+	private String damageType;
+	
+	public Damage fight(Character[] enemies){
+		return null;
+	}
+	
+	public void damageRecieved(Damage damage){
+		
+	}
+}
