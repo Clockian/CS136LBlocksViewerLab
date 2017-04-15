ef67b8b29d4a16815b904f23e0f6e3b4c510fe44
Jasque Saydyk <jrsaydyk@gmail.com>
2017-04-15 00:46:18 -0700

    :(

--- Lab 09/DoublyLinkedList.java
+++ Lab 09/DoublyLinkedList.java
@@ -6,39 +6,79 @@ import java.util.ListIterator;
 import java.util.ConcurrentModificationException;
 import java.util.NoSuchElementException;
 
-public class DoublyLinkedList<T> extends AbstractSequentialList<T> implements List<T>{
+public class DoublyLinkedList<E> extends AbstractSequentialList<E> implements List<E>{
 
-	private Node<T> firstNode;
-	private Node<T> lastNode;
+	private Node<E> firstNode;
+	private Node<E> lastNode;
+	private int size = 0;
+	
+	//private int modificationCount = 0;
 	
 	private class Node<E>{
-		public Node<E> next;
 		public Node<E> previous;
+		public Node<E> next;
 		public E data;
 		
-		
-		public Node(){
-			
-		}
-	}
-	
-	private class DLLIterator<E> implements ListIterator<E>{
-		private Node<E> inext;
-		private Node<E> iprev;
-		private Node<E> returned;
-		private int index;
-		private int imodcount;
-		
-		@Override
-		public void add(E arg0) {
-			// TODO Auto-generated method stub
-			
+		/**
+		 * Default Constructor to create a node
+		 * @param data - The data to be stored in the node
+		 */
+		public Node(Node<E> previous, Node<E> next, E data){
+			this.previous = previous;
+			this.next = next;
+			this.data = data;
 		}
 
+		public void setPrevious(Node<E> previous) {
+			this.previous = previous;
+		}
+		
+		public Node<E> getPrevious(){
+			return this.previous;
+		}
+		
+		public void setNext(Node<E> Next) {
+			this.next = next;
+		}
+		
+		public Node<E> getNext(){
+			return this.next;
+		}
+		
+		//public void setData(E data) {
+		//	this.data = data;
+		//}
+		
+		public E getData(){
+			return this.data;
+		}
+	}
+	/*
+	private class DoublyLinkedListIterator implements ListIterator<E>{
+		private Node<E> iNext;
+		private Node<E> iPrev;
+		private Node<E> returned;
+		private int index = 0;
+		private int iModificationsCount;
+		
+		public DoublyLinkedListIterator(){
+			index = 0;
+			iNext = firstNode;
+		}
+		
+		public DoublyLinkedListIterator(int num){
+			//index = num;
+			//iNext = firstNode;
+		}
+		
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
-			return false;
+			if(iNext == null){
+				return false;
+			}
+			iPrev = iNext;
+			iNext = iNext.getNext();
+			return true;
 		}
 
 		@Override
@@ -48,21 +88,9 @@ public class DoublyLinkedList<T> extends AbstractSequentialList<T> implements Li
 		}
 
 		@Override
-		public E next() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
 		public int nextIndex() {
 			// TODO Auto-generated method stub
 			return 0;
-		}
-
-		@Override
-		public E previous() {
-			// TODO Auto-generated method stub
-			return null;
 		}
 
 		@Override
@@ -78,149 +106,232 @@ public class DoublyLinkedList<T> extends AbstractSequentialList<T> implements Li
 		}
 
 		@Override
-		public void set(E arg0) {
-			// TODO Auto-generated method stub
+		public void add(E data) {
+			//if(size == 0){
+			//	Node<E> temp = new Node<>(data);
+			//	firstNode = temp; 
+			//	iNext = firstNode;
+			//	iPrev = null;
+			//}
 			
 		}
+
+		@Override
+		public E next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public E previous() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void set(E e) {
+			// TODO Auto-generated method stub
+			
+		}	
+	}*/
+	
+	/**
+	 * Default constructor for a LinkedList
+	 */
+	public DoublyLinkedList(){
+		size = 0;
+		firstNode = null;
+		lastNode = null;
+	}
+
+	/**
+	 * @return The number of elements in this list.
+	 */
+	@Override
+	public int size() {
+		return size;
+	}
+	
+
+
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		/*if(index < 0){
+			throw new IndexOutOfBoundsException();
+		}
+
+		DoublyLinkedListIterator it = new DoublyLinkedListIterator();
+		// Iterators are created at the beginning of the list by default.
+		// It must be advanced to the index parameter by hand
+		int actualIndex = 0;
+		int actualIndex1 = 0;
+		for(int ii = 0; ii < index; ii++){
+			actualIndex1++;
+			if(it.hasNext()){
+				actualIndex++;
+			}
+		}
+		System.out.println("actual: " + actualIndex + "  control: " + actualIndex1);
 		
+		if(actualIndex == index){
+			return it;
+		}
+		else{
+			throw new IndexOutOfBoundsException();
+		}*/
+		return null;
+	}
+	
+	/**
+	 * Returns the element at the specified position in this list.
+	 * Throws an IndexOutOfBoundsException when index is out of range.
+	 * @param index - Location of node to get data from
+	 * @return The element at the specified position
+	 */
+	@Override
+	public E get(int index) {
+		if(index < 0 || size == 0){
+			throw new IndexOutOfBoundsException();
+		}
+		Node<E> current = this.getFirstNode();
+		for(int ii = 0; ii != index; ii++){
+			current = current.getNext();
+		}
+		return current.getData();
+	}
+	
+	public DoublyLinkedList<E>.Node<E> getFirstNode() {
+		return firstNode;
 	}
 	
 	@Override
-	public boolean add(T arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	public void add(int index, E data) {
+		// If invalid index entry
+				if(index < 0){
+					throw new IndexOutOfBoundsException();
+				}
+				
+				// Starting a new list 
+				else if(size == 0){
+					// At the beginning
+					if(index == 0){
+						firstNode = new Node<>(null, null, data);
+						size++;
+						System.out.println("Working? 1");
+					}
+					
+					// The second spot
+					else if(index == 1){
+						firstNode = new Node<>(null, null, null);
+						lastNode = new Node<>(firstNode, null, data);
+						firstNode.setNext(lastNode);
+						size++;
+						size++;
+					}
+					
+					// Beyond
+					else{
+						firstNode = new Node<>(null, null, null);
+						lastNode = new Node<>(firstNode, null, null);
+						firstNode.setNext(lastNode);
+						size++;
+						size++;
+						// Make empty nodes till you get to the desired index
+						for(int ii = size; ii < index; ii++){
+							Node<E> newNode = new Node<>(lastNode, null, null);
+							lastNode.setNext(newNode);
+							lastNode = newNode;
+							size++;
+						}
+						// Loop ends at index to insert object
+						Node<E> newNode = new Node<>(lastNode, null, data);
+						lastNode.setNext(newNode);
+						lastNode = newNode;
+						size++;
+					}
+				}
+				
+				// If new item is to be put at the end of the list
+				else if(size == index){
+					// If there is only the head
+					if(size == 1){
+						lastNode = new Node<>(firstNode, null, data);
+						firstNode.setNext(lastNode);
+						size++;
+						System.out.println("Working? 2");
+					}
+					// Otherwise, add at end of the list
+					else{			
+						Node<E> newNode = new Node<>(lastNode, null, data);
+						lastNode.setNext(newNode);
+						lastNode = newNode;
+						size++;
+						System.out.println("Working? 3");
+					}
+				}
+				
+				// If new item is to be put in slot bigger than already created list
+				else if(size < index){
+					// If there is only the head
+					if(size == 1){
+						lastNode = new Node<>(firstNode, null, null);
+						firstNode.setNext(lastNode);
+						size++;
+					}
+					
+					// Make empty nodes till you get to the desired index
+					for(int ii = size; ii < index; ii++){
+						Node<E> newNode = new Node<>(lastNode, null, null);
+						lastNode.setNext(newNode);
+						lastNode = newNode;
+						size++;
+					}
+					// Loop ends at index to insert object
+					Node<E> newNode = new Node<>(lastNode, null, data);
+					lastNode.setNext(newNode);
+					lastNode = newNode;
+					size++;
+				}
+				
+				// Adding inside of list(push all numbers back, append to location)
+				else if(size > index){
+					// If appending to head with no tail
+					if(index == 0 && size == 1){
+						Node<E> newNode = new Node<>(null, firstNode, data);
+						firstNode.setPrevious(newNode);
+						lastNode = firstNode;
+						firstNode = newNode;
+						size++;
+					}
+					// If appending to head
+					else if(index == 0){
+						Node<E> newNode = new Node<>(null, firstNode, data);
+						firstNode.setPrevious(newNode);
+						firstNode = newNode;
+						size++;
+					}
+					// If appending in body
+					else{
+						// Find node, when found, add the node
+						Node<E> current = firstNode.getNext();
+						for(int ii = 1; ii < size; ii++){
+							if(ii == index){
+								Node<E> newNode = new Node<>(current.getPrevious(), current, data);
+								current.previous.setNext(newNode);
+								current.setPrevious(newNode);
+								size++;
+								break;
+							}
+							else{
+								current = current.getNext();
+							}
+						}
+					}
+				}
+				
+				else{
+					System.out.println("Something went wrong in adding to array!");
+				}
 	}
-
-	@Override
-	public void add(int arg0, T arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean addAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addAll(int arg0, Collection arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void clear() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean contains(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean containsAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public T get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int indexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Iterator iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int lastIndexOf(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public ListIterator listIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ListIterator listIterator(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean remove(Object arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public T remove(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean removeAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean retainAll(Collection arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Object set(int arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public List subList(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object[] toArray(Object[] arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- Lab 09/DoublyLinkedListTest.java
+++ Lab 09/DoublyLinkedListTest.java
@@ -1,6 +1,66 @@
+import java.util.ListIterator;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class DoublyLinkedListTest {
+	DoublyLinkedList<Integer> test;
 	
+	@Before
+	public void setup(){
+		test = new DoublyLinkedList<>();
+	}
+	/*
+	@Test
+	public void testInitialization(){
+		
+	}
+	
+	@Test
+	public void retrieveDefaultIteratorObject(){
+		ListIterator<Integer> iter = test.listIterator();
+		Assert.assertNotEquals(iter, null);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void retrieveCustomIteratorObjectThrowsOutOfBoundsException(){
+		ListIterator<Integer> iter = test.listIterator(4);
+	}
+	*/
+	@Test
+	public void retrieveCustomIteratorObject(){
+		DoublyLinkedList<Integer> test1 = new DoublyLinkedList<>();
+		
+		System.out.println("\nRetrieveCustomIteratorObject");
+		for(Integer ii = 0; ii < 6; ii++){
+			System.out.println("add(ii): " + ii);
+			test1.add(ii, ii);
+		}
+		
+		System.out.println();
+		for(Integer ii = 0; ii < 6; ii++){
+			System.out.print("Pre result: " + test1.get(ii) + ", ");
+		}
+		
+		ListIterator<Integer> iter = test1.listIterator(4);
+		
+		System.out.println();
+		for(Integer ii = 0; ii < 6; ii++){
+			System.out.print("Post result: " + test1.get(ii) + ", ");
+		}
+		
+		System.out.println("\n");
+		//Integer answer = 4;
+		//Assert.assertEquals(answer, test.next());
+	}
+	/*
+	@Test
+	public void addToEmptyList(){
+		DoublyLinkedList<Integer> test1 = new DoublyLinkedList<>();
+		ListIterator<Integer> iter = test1.listIterator();
+		iter.add(55);
+		Integer answer = 55;
+		Assert.assertEquals(answer, test1.get(0));
+	}*/
 }
